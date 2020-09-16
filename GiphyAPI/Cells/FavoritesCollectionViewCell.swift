0e831371da261e277170b8c62cf5ddc57aebd3f8
Zipei Jerry Shi <jerryjerryszp@gmail.com>
2020-09-16 15:19:21 -0400

    Add favorites collection view cell

---
+++ GiphyAPI/Cells/FavoritesCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  FavoritesCollectionViewCell.swift
+//  GiphyAPI
+//
+//  Created by Jerry Shi on 2020-09-16.
+//  Copyright © 2020 jerryszp6116. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FavoritesCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var collectionView: UIImageView!
+    
+    
+    @IBOutlet weak var unfavoriteButton: UIButton!
+    
+    @IBAction func unfavorite(_ sender: Any) {
+        
+        
+    }
+    
+}
